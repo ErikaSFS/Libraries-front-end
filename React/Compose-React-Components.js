1e03ce18edcfa65f422e4abd128966c832d7a60f
Erika Faustino <dossantos.erika08@gmail.com>
2020-03-27 22:36:16 -0300

    Create  Compose-React-Components.js

---
+++ React/Compose-React-Components.js
@@ -0,0 +1,16 @@
+class Fruits extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+        { /* change code below this line */ }
+  <NonCitrus />
+  <Citrus />
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
