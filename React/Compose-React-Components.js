abd138e5d3666475a13ef6da61471b2ea1f495bf
Erika Faustino <dossantos.erika08@gmail.com>
2020-03-27 22:41:49 -0300

    Update Compose-React-Components.js

--- React/Compose-React-Components.js
+++ React/Compose-React-Components.js
@@ -6,10 +6,25 @@ class Fruits extends React.Component {
     return (
       <div>
         <h2>Fruits:</h2>
-        { /* change code below this line */ }
   <NonCitrus />
   <Citrus />
-        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+     super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        
+        <Fruits />
+        <Vegetables />
+        
       </div>
     );
   }
